0fa916dcdd3d86d6e4214653fb54622b113fdafb
Watermelon <62407841+StarxSky@users.noreply.github.com>
2022-08-14 10:18:22 +0800

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,37 @@
+import torch
+import numpy as np
+from scipy.io.wavfile import read
+
+
+from hparams import create_hparams
+hparams = create_hparams()
+
+def get_mask_from_lengths(lengths):
+    max_len = torch.max(lengths).item()
+    if hparams.cuda_enabled :
+        ids = torch.arange(0, max_len, out=torch.cuda.LongTensor(max_len))
+        mask = (ids < lengths.unsqueeze(1)).bool()
+    else :
+        ids = torch.arange(0, max_len, out=torch.LongTensor(max_len))
+        mask = (ids < lengths.unsqueeze(1)).bool()
+    
+    return mask
+
+
+def load_wav_to_torch(full_path):
+    sampling_rate, data = read(full_path)
+    return torch.FloatTensor(data.astype(np.float32)), sampling_rate # 这里的参数直接关联到hparams文件的参数设置
+
+
+def load_filepaths_and_text(filename, split="|"):
+    with open(filename, encoding='utf-8') as f:
+        filepaths_and_text = [line.strip().split(split) for line in f]
+    return filepaths_and_text
+
+# 调用GPU进行机算
+def to_gpu(x):
+    x = x.contiguous()
+
+    if torch.cuda.is_available():
+        x = x.cuda(non_blocking=True)
+    return torch.autograd.Variable(x)
